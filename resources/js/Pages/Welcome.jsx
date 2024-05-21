372dff8f23d446445efe3b621ca9a5bb4e3a0d6c
Damaris Jean Jorge Manzano <damarisjean@Damariss-MacBook-Pro.local>
2024-05-21 13:49:20 +0200

    adding changes to welcome

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -8,11 +8,15 @@ export default function Welcome({ auth }) {
     return (
         <>
             <Head title="Welcome" />
-            <div
-                className="h-screen bg-cover bg-center flex flex-col justify-between "
-                style={{ backgroundImage: "url(/images/background2.jpg)" }}
-            >
-                <div className="flex-0 flex flex-col justify-center items-center text-center">
+            <div className="relative h-screen flex flex-col justify-between overflow-hidden">
+                <div
+                    className="absolute inset-0 bg-cover bg-center"
+                    style={{ backgroundImage: "url(/images/background2.jpg)" }}
+                >
+                    {/* Blur Effect */}
+                    <div className="absolute inset-0 bg-gradient-to-b from-transparent to-[#DFDAD6]"></div>
+                </div>
+                <div className="relative flex-0 flex flex-col justify-center items-center text-center">
                     <Link href="/">
                         <ApplicationLogo className="h-60 mb-5" />
                     </Link>
@@ -24,18 +28,17 @@ export default function Welcome({ auth }) {
 
                     <p className="mt-6 text-2xl leading-8 text-[#2a4047]">
                         Light finds its way to our hearts every day,
-                        <strong>Read the stories</strong> of others and
-                        <strong>Share your own.</strong>
+                        <strong> Read the stories</strong> of others and
+                        <strong> Share your own.</strong>
                     </p>
                     <div className="flex flex-row gap-32 mt-20">
-                        <div className=" text-[#ffffff] px-32 py-4 rounded-full tracking-widest uppercase font-bold bg-[#2a4047] hover:text-[#ffffff] dark:text-[#ffffff] transition duration-200 transform hover:scale-110">
+                        <div className="text-[#ffffff] px-32 py-4 rounded-full tracking-widest uppercase font-bold bg-[#2a4047] hover:text-[#ffffff] dark:text-[#ffffff] transition duration-200 transform hover:scale-110">
                             {auth.user ? (
                                 <Link href={route("dashboard")}>
                                     START READING HERE
                                 </Link>
                             ) : (
                                 <Link href={route("register")}>
-                                    {" "}
                                     START READING HERE
                                 </Link>
                             )}
