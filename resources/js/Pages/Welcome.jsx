e9e64a795f67926eb4b01671574a89de2d75f8f1
Damaris Jean Jorge Manzano <damarisjean@Damariss-MacBook-Pro.local>
2024-05-26 16:02:13 +0200

    addming changes to welcome.jsx

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,5 +1,4 @@
 import { Link, Head } from "@inertiajs/react";
-import NavLink from "@/Components/Navigation/NavLink";
 import ApplicationLogo from "@/Components/Logo/ApplicationLogo";
 import MiraclesCarousel from "../Components/Carousel/MiraclesCarousel";
 import Footer from "../Components/Footer/Footer";
@@ -9,41 +8,40 @@ export default function Welcome({ auth }) {
         <>
             <Head title="Welcome" />
 
-            <div
-                className="h-screen bg-cover bg-center flex flex-col justify-between "
-                style={{ backgroundImage: "url(/images/background2.jpg)" }}
-            >
-                <div className="flex-0 flex flex-col justify-center items-center text-center">
+            <div className="relative h-screen flex flex-col justify-between overflow-hidden">
+                <div
+                    className="absolute inset-0 bg-cover bg-center"
+                    style={{ backgroundImage: "url(/images/background2.jpg)" }}
+                >
+                    <div className="absolute inset-0 bg-gradient-to-b from-transparent to-[#DFDAD6]"></div>
+                </div>
+                <div className="relative flex-0 flex flex-col justify-center items-center text-center">
                     <Link href="/">
                         <ApplicationLogo className="h-60 mb-5" />
                     </Link>
-                    <h2 className="text-6xl font-bold tracking-tight text-[#2a4047] sm:text-7xl">
-                        Experience Miracles.
-                        <br />
+                    <h2 className="text-6xl font-bold tracking-tight text-[#2a4047] sm:text-7xl ">
                         Start Writing Today.
+                        <br />
+                        Experience Miracles.
                     </h2>
 
-                    <p className="mt-6 text-2xl leading-8 text-[#2a4047]">
-                        Light finds its way to our hearts every day,{" "}
-                        <strong>Read the stories</strong> of others and{" "}
-                        <strong>Share your own.</strong>
+                    <p className="mt-6  text-2xl leading-8 text-[#2a4047]">
+                        Light finds its way to our hearts every day,
+                        <strong> Read the stories</strong> of others and
+                        <strong> Share your own.</strong>
                     </p>
-                    <div className="mt-10">
-                        {auth.user ? (
-                            <Link
-                                href={route("dashboard")}
-                                className="inline-flex items-center px-6 py-3 bg-[#2a4047] border border-transparent rounded-md font-semibold text-sm text-white uppercase tracking-widest hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-in-out duration-150"
-                            >
-                                START HERE
-                            </Link>
-                        ) : (
-                            <Link
-                                href={route("login")}
-                                className="inline-flex items-center px-6 py-3 bg-[#2a4047] border border-transparent rounded-md font-semibold text-sm text-white uppercase tracking-widest hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-in-out duration-150"
-                            >
-                                START HERE
-                            </Link>
-                        )}
+                    <div className="flex flex-row gap-32 mt-20">
+                        <div className="text-[#ffffff] px-32 py-4 rounded-full tracking-widest uppercase font-bold bg-[#2a4047] hover:text-[#ffffff] dark:text-[#ffffff] transition duration-200 transform hover:scale-110">
+                            {auth.user ? (
+                                <Link href={route("dashboard")}>
+                                    START READING HERE
+                                </Link>
+                            ) : (
+                                <Link href={route("register")}>
+                                    START READING HERE
+                                </Link>
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
